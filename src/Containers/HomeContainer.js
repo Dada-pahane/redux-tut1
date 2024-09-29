e394a2c6b4f045429a8c93639487f9445deb37b9
Dada-pahane <dadasahebpahane@gmail.com>
2024-09-29 20:05:43 +0530

    in Containe Folder Connt React Compont and Redux Using react-redux

--- src/Containers/HomeContainer.js
+++ src/Containers/HomeContainer.js
@@ -1,3 +1,18 @@
 import Home from '../Components/Home'
+import {connect} from 'react-redux'
+import {addToCart} from '../Services/Actions/Action'
 
-export default Home;
+
+
+const  mapStateToProps= state=>({
+
+})
+const mapDispatchToProps=dispatch=>({
+    addToCartHandel:data=> dispatch(addToCart(data))
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(Home)
+
+
+
+// export default Home;
